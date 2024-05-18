052727c4c898523a9f5940917cc2f204fb1610b9
OmyK <31337777+icons88@users.noreply.github.com>
2024-05-18 17:31:40 +0800

    Update DIY-2.sh

--- 6.1/DIY-2.sh
+++ 6.1/DIY-2.sh
@@ -2,31 +2,34 @@
 #============================================================================================
 
 
-# 0-设置主机名
+# 设置主机名
 # sed -i 's/OpenWrt/MopWrt/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='OpenWrt'/hostname='MopWrt'/g" package/base-files/files/bin/config_generate
 
-# 1-设置默认主题
+# 设置默认主题
 sed -i 's/bootstrap/opentomcat/g' ./feeds/luci/collections/luci/Makefile
 
-# 2-设置管理地址
+# 设置管理地址
 sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
 
-# 3-编译内核版本
+# 编译内核版本
 # sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
-# 4-设置密码为空
+# 设置密码为空
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
-# 5-修改时间格式
+# 修改时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
-# 6-添加固件日期
+# 添加固件日期
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
 sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
 
-# 7-修正硬件信息
+# 修正硬件信息
 sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
 
-# 8-增固件连接数
+# 增固件连接数
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
+# 禁用ipv6前缀
+sed -i 's/^[^#].*option ula/#&/' /etc/config/network
