bf8b9007bf723ea9c19e7f326470ad38508d4723
OmyK <31337777+icons88@users.noreply.github.com>
2024-06-25 01:37:59 +0800

    Update DIY-1.sh

--- 6.1/DIY-1.sh
+++ 6.1/DIY-1.sh
@@ -20,3 +20,6 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 # 添加PassWall插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 添加uugamebooster插件
+git clone https://github.com/kiddin9/openwrt-packages.git package/luci-app-uugamebooster
