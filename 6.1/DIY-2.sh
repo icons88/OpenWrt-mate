c575fb5d4f943a8d7f03445bf9426a42765461b8
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-11 22:55:06 +0800

    Update DIY-2.sh

--- 6.1/DIY-2.sh
+++ 6.1/DIY-2.sh
@@ -6,6 +6,9 @@
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
+# 修改登录IP
+sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/luci2/bin/config_generate
+
 # 编译内核版本
 # sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
