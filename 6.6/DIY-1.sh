5bddca98e9211b0548fe3a7b2079d1cc7be3e668
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-19 19:19:57 +0800

    Update DIY-1.sh

--- 6.6/DIY-1.sh
+++ 6.6/DIY-1.sh
@@ -1,22 +1,9 @@
-# 此脚本用处是：添加第三方插件
-#=========================================================================================================================
-
-
-# 添加ShadowSocksR Plus+插件
+# ShadowSocksR Plus+
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 添加PowerOff关机插件
-git clone https://github.com/TraderWukong/luci-app-poweroff.git package/luci-app-poweroff
-
-# 添加Opentomcat主题
-git clone https://github.com/icons88/luci-theme-opentomcat.git package/luci-theme-opentomcat
-
-# 添加adguardhome插件
+# adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
-# 添加OpenClash插件
-sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
-
-# 添加PassWall插件
+# PassWall
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
