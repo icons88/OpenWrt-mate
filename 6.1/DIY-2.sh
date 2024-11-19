086719b1e72013d7be69256bea789cf518d528ef
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-19 19:23:40 +0800

    Update DIY-2.sh

--- 6.1/DIY-2.sh
+++ 6.1/DIY-2.sh
@@ -1,30 +1,9 @@
-# 此脚本用处是：定制个性化参数
-#============================================================================================
-
-
-# 替换新版 golang
+# golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-# 编译内核版本
-# sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+# kernel
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
 
-# 修改登录IP
-sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/luci2/bin/config_generate
-
-# 设置密码为空
+# password
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
-
-# 修改时间格式
-sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
-
-# 添加固件日期
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
-sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
-
-# 修正硬件信息
-sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
-
-# 增固件连接数
-sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
