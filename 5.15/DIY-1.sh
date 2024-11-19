a09dd9ac703a13305e1f7925859c19a30a69d0ab
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-19 19:20:40 +0800

    Update DIY-1.sh

--- 5.15/DIY-1.sh
+++ 5.15/DIY-1.sh
@@ -1,9 +1,9 @@
-# 添加ShadowSocksR Plus+插件
+# ShadowSocksR Plus+
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 添加adguardhome插件
+# adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
-# 添加PassWall插件
+# PassWall
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
