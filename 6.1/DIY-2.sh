c227158667632a78baa7f07d185740fddffffe0d
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-26 07:49:18 +0800

    Update DIY-2.sh

--- 6.1/DIY-2.sh
+++ 6.1/DIY-2.sh
@@ -1,12 +1,9 @@
 # golang
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # kernel
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
-
-sed -i '/openwrt-23.05/d' feeds.conf.default
-sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
 
 # password
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
