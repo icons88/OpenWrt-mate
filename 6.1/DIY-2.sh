3e02a7cd1f5c657904265d66f6c5101cdc66f0c5
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-28 19:23:14 +0800

    Update DIY-2.sh

--- 6.1/DIY-2.sh
+++ 6.1/DIY-2.sh
@@ -7,3 +7,6 @@ sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefi
 
 # password
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+
+# Modify default IP
+sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
