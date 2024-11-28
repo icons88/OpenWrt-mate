63460bb53e54cd536990d7348bc603d7a8669d0b
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-28 19:27:26 +0800

    Update DIY-2.sh

--- 6.1/DIY-2.sh
+++ 6.1/DIY-2.sh
@@ -1,12 +1,13 @@
-# golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-# kernel
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
 
-# password
+sed -i "s/hostname='OpenWrt'/hostname='MinGwrt'/g" package/base-files/files/bin/config_generate
+
+sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
+
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
-# Modify default IP
-sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
