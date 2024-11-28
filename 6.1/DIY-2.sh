9dedf24acbd16b2b26a2358ffe7c05021d88d1ec
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-28 23:52:26 +0800

    Update DIY-2.sh

--- 6.1/DIY-2.sh
+++ 6.1/DIY-2.sh
@@ -8,6 +8,3 @@ sed -i "s/hostname='OpenWrt'/hostname='MinGwrt'/g" package/base-files/files/bin/
 sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
-
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
-sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
