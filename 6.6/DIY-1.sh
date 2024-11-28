b098d1f950a474459ce248b2f2d30d2a2e0ee751
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-28 23:53:04 +0800

    Update DIY-1.sh

--- 6.6/DIY-1.sh
+++ 6.6/DIY-1.sh
@@ -1,12 +1,8 @@
-# ShadowSocksR Plus+
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-# adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
-# poweroff
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 
-# PassWall
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
