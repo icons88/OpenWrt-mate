314e49189679c54f4399a304027cf15bb08e64ec
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-29 21:47:09 +0800

    Update DIY-2.sh

--- 5.15/DIY-2.sh
+++ 5.15/DIY-2.sh
@@ -1,10 +1,9 @@
-# golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-# kernel
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
-# password
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
