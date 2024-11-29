f92734a8640457f91236e241493e5ec6c6d0492b
OmyK <31337777+icons88@users.noreply.github.com>
2024-11-29 15:51:44 +0800

    Delete 6.1/diy-part.sh

--- 6.1/diy-part.sh
+++
@@ -1,8 +0,0 @@
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-
-sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
-
-sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
-
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
