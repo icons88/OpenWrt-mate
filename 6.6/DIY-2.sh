4f8217095ffdba0696a436b5605a3465bbd8124b
OmyK <31337777+icons88@users.noreply.github.com>
2024-12-02 14:23:03 +0800

    Update DIY-2.sh

--- 6.6/DIY-2.sh
+++ 6.6/DIY-2.sh
@@ -1,8 +1,5 @@
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+# sed -i "s/hostname='OpenWrt'/hostname='MinGwrt'/g" package/base-files/files/bin/config_generate
 
-sed -i "s/hostname='OpenWrt'/hostname='MinGwrt'/g" package/base-files/files/bin/config_generate
-
-sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
