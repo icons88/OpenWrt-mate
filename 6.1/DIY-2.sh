adc3bdc0b9e9e46084c64f52318a25505025604c
OmyK <31337777+icons88@users.noreply.github.com>
2024-12-13 21:00:42 +0800

    Update DIY-2.sh

--- 6.1/DIY-2.sh
+++ 6.1/DIY-2.sh
@@ -5,7 +5,7 @@ sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='LEDE'/hostname='MinGwrt'/g" package/base-files/files/bin/config_generate
 
 # 修改内核版本
-sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
 
 # 删除默认密码
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
