3ff43b5adc68bf7e629777c5bec95a7f67f56322
OmyK <31337777+icons88@users.noreply.github.com>
2024-12-27 15:00:15 +0800

    Create DIY-2.sh

---
+++ 6.12/DIY-2.sh
@@ -0,0 +1,24 @@
+# 修改管理地址
+sed -i 's/192.168.1.1/100.1.1.1/g' package/base-files/files/bin/config_generate
+
+# 修改主机名称
+sed -i "s/hostname='LEDE'/hostname='MinGwrt'/g" package/base-files/files/bin/config_generate
+
+# 修改内核版本
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
+
+# 删除默认密码
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+
+# 修改默认主题
+# sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' feeds/luci/collections/luci/Makefile
+
+# 修改时间格式
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+
+# 添加编译日期
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
+
+# 只显示CPU型号
+sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
