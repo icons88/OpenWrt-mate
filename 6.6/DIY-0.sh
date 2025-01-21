53ae3ec99beceedacc993e32ac9dc0be58baa205
OmyK <31337777+icons88@users.noreply.github.com>
2025-01-21 13:55:45 +0800

    Update DIY-0.sh

--- 6.6/DIY-0.sh
+++ 6.6/DIY-0.sh
@@ -8,5 +8,5 @@ git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app
 # git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
 # 锁定 PassWall 到 4.78 - 4 版本
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;4.78-4" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;4.78-4" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/icons88/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
