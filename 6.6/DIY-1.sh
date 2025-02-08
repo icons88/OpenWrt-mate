7b481b143a95545b59db95ff4065335418e37a26
OmyK <31337777+icons88@users.noreply.github.com>
2025-02-08 14:42:39 +0800

    Update DIY-1.sh

--- 6.6/DIY-1.sh
+++ 6.6/DIY-1.sh
@@ -15,4 +15,5 @@ git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app
 
 # 添加 PassWall 插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
